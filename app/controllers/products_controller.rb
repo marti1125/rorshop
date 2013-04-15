f500213f632e87039e4a52588c4490f329b44fb8
Willy Aguirre <marti1003@hotmail.com>
2013-04-15 15:24:21 -0500

    add action products

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -3,15 +3,35 @@ class ProductsController < ApplicationController
 		@products = Product.all
 	end
 	def show
+		@product = Product.find(params[:id])
 	end
 	def new
+		@types = Type.all
+		@product = Product.new
 	end
 	def create
+		@type = Type.find(params[:type_id])
+		@product = @type.products.new(params[:product])
+		if @product.save
+			redirect_to :action => :show, :id => @product.id
+		else
+			render 'new'
+		end
 	end
 	def edit
+		@product = Product.find(params[:id])
 	end
 	def update
+		@product = Product.find(params[:id])
+		if @product.update_attributes(params[:product])
+			redirect_to :action => :show, :id => @product.id
+		else
+			render 'edit'
+		end
 	end
 	def destroy
+		@product = Product.find(params[:id])
+		@product.destroy
+		redirect_to :action => :index
 	end
 end
