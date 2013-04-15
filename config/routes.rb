192fbf70a1f85518ced741f915e9dcb8b02e0957
Willy Aguirre <marti1003@hotmail.com>
2013-04-15 00:08:43 -0500

    add product controller model and main site

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rorshop::Application.routes.draw do
-  resources :products
 
+  resources :products
 
   resources :types
 
@@ -54,7 +54,7 @@ Rorshop::Application.routes.draw do
 
   # You can have the root of your site routed with "root"
   # just remember to delete public/index.html.
-  # root :to => 'welcome#index'
+  root :to => 'products#index'
 
   # See how all your routes lay out with "rake routes"
 
