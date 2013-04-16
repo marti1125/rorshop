da027b8917994dadc5016c536b6dfc8347d33b94
Willy Aguirre <marti1003@hotmail.com>
2013-04-16 14:59:58 -0500

    add controller welcomes

--- config/routes.rb
+++ config/routes.rb
@@ -53,7 +53,7 @@ Rorshop::Application.routes.draw do
 
   # You can have the root of your site routed with "root"
   # just remember to delete public/index.html.
-  root :to => 'products#index'
+  root :to => 'welcomes#index'
 
   # See how all your routes lay out with "rake routes"
 
