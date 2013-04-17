9ada8909b99a2c0476deaaebf000e569a905c488
Willy Aguirre <marti1003@hotmail.com>
2013-04-17 17:05:51 -0500

    modify project

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -10,8 +10,8 @@ class ProductsController < ApplicationController
 		@product = Product.new
 	end
 	def create
-		#@type = Post.find(params[:post_id])
-		@product = Product.new(params[:product])
+		#@type = Type.find(params[:type_id])
+		@product = Product.create(params[:product])
 		if @product.save
 			redirect_to :action => :show, :id => @product.id
 		else
--- app/models/product.rb
+++ app/models/product.rb
@@ -1,4 +1,4 @@
 class Product < ActiveRecord::Base
   belongs_to :type
-  attr_accessible :amount, :description
+  attr_accessible :type_id, :amount, :description
 end
